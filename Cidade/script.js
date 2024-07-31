6e5862c0a16c05cf03343c4e01a6cbb73d9f90e5
HiagoAnastacio <159261628+HiagoAnastacio@users.noreply.github.com>
2024-07-31 11:37:46 -0300

    html

---
+++ Cidade/script.js
@@ -0,0 +1,22 @@
+let imagens = document.getElementById('img1')
+let legendas = document.getElementById('legenda')
+function image1()
+{
+    imagens.src="Berlim/img1.jpg"
+    legendas.innerHTML="Vista panorâmica do centro de Berlim."
+}
+function image2()
+{
+    imagens.src="Berlim/img2.jpg"
+    legendas.innerHTML="Vista noturna da cidade de Berlim."
+}
+function image3()
+{
+    imagens.src="Berlim/img3.jpg"
+    legendas.innerHTML="Uma foto do marco histórico que é o muro de Berlim."
+}
+function image4()
+{
+    imagens.src="Berlim/img4.jpg"
+    legendas.innerHTML="Literamente eu montando esse site tendo faltado os dias onde todo o conteúdo foi feito."
+}
